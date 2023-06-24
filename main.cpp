10921da09c5e04050170dd18dd56e50bcc795617
basement_mustard <120338237+TheGriddyGitty@users.noreply.github.com>
2023-06-23 18:39:38 -0600

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int main() {
+  std::cout << "Prepare..." << std::endl;
+  return 0;
+}  
+
+#include <iostream>
+
+int main() {
+  std::cout << "For a new challenge..." << std::endl;
+  return 0;
+}  
+
+#include <iostream>
+
+int main() {
+  std::cout << "And a new suffering..." << std::endl;
+  return 0;
+}  
+
+std::string name;
+
+std::cout << "What is your character's name : " << std::endl;
+std::cin >> name;
+
+std::cout << "Welcome " << name << ". Get ready for the new adventure..." << std::endl;
